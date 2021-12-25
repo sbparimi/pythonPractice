518592ab74d2b846813d9e69c6ac539dc66c6f2d
sbparimi <reachparimi@gmail.com>
2021-12-25 10:41:24 +0530

    list comprehensions 2

--- lists.py
+++ lists.py
@@ -1,4 +1,16 @@
+# print even numbers using list comprehension
 numbers = [1,2,3,4,5,6,7,8]
 result = [n * n for n  in numbers if n%2==0]
-print(result)
+#print(result)
+
+# print (letter pair) for abcd for each number in 0123 
+PairedList = []
+for eachLetter in "abcd":
+    for eachNumber in "0123":
+        PairedList.append((eachLetter, int(eachNumber)))
+print(PairedList)
+
+PairedList2 = [(eachLetter,eachNumber) for eachLetter in "abcd" for eachNumber in range(4) ]
+print("This is the Paired list 2", PairedList2)
+
 
